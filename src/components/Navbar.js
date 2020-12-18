895661088f4c600cad62b070ca64fd3bc3101d43
Dwan72 <alex.cap.an@gmail.com>
2020-12-17 22:26:23 -0600

    creating the Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import bootstrap from 'bootstrap'
+import React, {useState, useEffect} from 'react';
+
+
+function Navbar() {
+
+    return(
+        <div>
+            
+
+
+        </div>
+    )
+
+}
+
+
+export default Navbar;
